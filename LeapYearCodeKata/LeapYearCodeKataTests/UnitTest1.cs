93e37d25cf64c77c854809c75900f14718a407c3
Raul Naupari <raul.naupari@tranzact.net>
2021-05-17 21:37:44 -0500

    it_is_not_leap_year_when_is_not_divisible_by_4 in red

--- LeapYearCodeKata/LeapYearCodeKataTests/UnitTest1.cs
+++ LeapYearCodeKata/LeapYearCodeKataTests/UnitTest1.cs
@@ -15,5 +15,15 @@ namespace LeapYearCodeKataTests
 
             Assert.IsTrue(isLeapYear);
         }
+
+        [TestMethod]
+        public void it_is_not_leap_year_when_is_not_divisible_by_4()
+        {
+            var checker = new LeapYearChecker();
+
+            var isLeapYear = checker.IsLeap(2001);
+
+            Assert.IsFalse(isLeapYear);
+        }
     }
 }
