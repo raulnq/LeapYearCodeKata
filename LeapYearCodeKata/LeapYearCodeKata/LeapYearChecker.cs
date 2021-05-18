87a1d7e2080ffbd794cb78b9e367c0e80bc40137
Raul Naupari <raul.naupari@tranzact.net>
2021-05-18 11:13:43 -0500

    it_is_not_leap_year_when_is_divisible_by_4_and_100 in green

--- LeapYearCodeKata/LeapYearCodeKata/LeapYearChecker.cs
+++ LeapYearCodeKata/LeapYearCodeKata/LeapYearChecker.cs
@@ -4,6 +4,11 @@
     {
         public bool IsLeap(int year)
         {
+            if(year % 100 == 0)
+            {
+                return false;
+            }
+
             return year % 4 == 0;
         }
     }
